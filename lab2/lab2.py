9f3810fb5345ec96f771299dd3523fc8c9547bbb
YongLiang <ngyongliang1@gmail.com>
2025-11-23 18:30:03 +0800

    Lab2 added BMI calculation

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,1 +1,20 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to python")
+
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("weight = " + str(weight))
+    
+    bmi = weight/(height*height)
+    print("BMI = " + str(bmi))
+    if bmi < 18.5:
+        print("under weight")
+    elif bmi > 25.0:
+        print("Over weight")
+    else:
+        print("Normal Weight")
+    
+calculate_bmi(weight = 57, height = 1.73)
+
+
+
+
